89cdc28eec8a00c7ffbea31f6aa5312774b95f94
Amy <duyao.dy1993@gmail.com>
2016-05-03 22:15:16 +0800

    first commit

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,35 @@
+package com.taotao.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
+import com.taotao.service.ItemService;
+
+@Service
+public class ItemServiceImpl implements ItemService{
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+
+	@Override
+	public TbItem getItemById(Long id) {
+		//1.
+//		TbItem item = tbItemMapper.selectByPrimaryKey(id);
+		//2.
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(id);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if(list != null && list.size() > 0){
+			return list.get(0);
+		}
+		return null;
+	}
+
+}
